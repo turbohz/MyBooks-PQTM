76c418ac8e62b4e8277a9fba17e839ce83658a9a
Gerard Ruiz <gruizto@uoc.edu>
2018-10-16 13:59:41 +0200

    Implements Firebase sign in

--- app/src/main/java/edu/uoc/gruizto/mybooks/activity/BookListActivity.java
+++ app/src/main/java/edu/uoc/gruizto/mybooks/activity/BookListActivity.java
@@ -5,15 +5,18 @@ import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -22,13 +25,29 @@ import edu.uoc.gruizto.mybooks.fragment.BookDetailFragment;
 import edu.uoc.gruizto.mybooks.model.BookItem;
 import edu.uoc.gruizto.mybooks.model.BookRepository;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class BookListActivity extends AppCompatActivity {
 
+    private static final String TAG = BookListActivity.class.getName();
     /**
      * Whether or not the activity is in two-pane mode, i.e. running on a tablet
      * device.
      */
     private boolean mTwoPane;
+
+    private FirebaseAuth mAuth;
+    private FirebaseDatabase database;
+
+    // FIXME: this should not be hardcoded, and secrets should not be stored in the app
+    private static final String USER_EMAIL = "gruizto@uoc.edu";
+    private static final String USER_PASSWORD = "QhW6Yk97sjvNr";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,6 +92,33 @@ public class BookListActivity extends AppCompatActivity {
                         mTwoPane
                 )
         );
+
+        // Connect to Firebase database
+
+        final BookListActivity activity = this;
+
+        mAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+
+        mAuth.signInWithEmailAndPassword(USER_EMAIL, USER_PASSWORD)
+            .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(BookListActivity.TAG, "signInWithEmail:success");
+                        Toast.makeText(activity, "Authentication succeeded.", Toast.LENGTH_SHORT).show();
+                        FirebaseUser user = task.getResult().getUser();
+
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(BookListActivity.TAG, "signInWithEmail:failure", task.getException());
+                        Toast.makeText(activity, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                    }
+
+                    // ...
+                    }
+        });
     }
 
     public static class SimpleItemRecyclerViewAdapter
