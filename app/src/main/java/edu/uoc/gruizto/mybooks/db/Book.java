d13ef571040f883c89678c62efb7588e2dcb71f6
Gerard Ruiz <gruizto@uoc.edu>
2018-10-18 17:24:14 +0200

    Updates book to become an entity

--- app/src/main/java/edu/uoc/gruizto/mybooks/db/Book.java
+++ app/src/main/java/edu/uoc/gruizto/mybooks/db/Book.java
@@ -1,24 +1,20 @@
 package edu.uoc.gruizto.mybooks.db;
 
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+@Entity(tableName = "mybooks")
 public class Book {
 
-    private static final String DEFAULT_COVER_URL = "https://i.pinimg.com/564x/27/7a/0f/277a0f2733bc01ec7710a51faca1de31.jpg";
-
+    @PrimaryKey
+    @NonNull
     public String id;
     public String title;
     public String author;
     public String publicationDate;
     public String description;
     public String coverUrl;
-
-    public Book() {
-        this.id = "0";
-        this.title = "Default title";
-        this.author = "Default author";
-        this.publicationDate = "17/10/2018";
-        this.description = "Default description";
-        this.coverUrl = DEFAULT_COVER_URL;
-    }
 
     @Override
     public String toString() {
