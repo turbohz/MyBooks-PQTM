229cd3b572943075620d3c79c3efed91f5b478d5
Gerard Ruiz <gruizto@uoc.es>
2018-11-09 20:42:41 +0100

    WIP

--- app/src/main/java/edu/uoc/gruizto/mybooks/activity/BookListActivity.java
+++ app/src/main/java/edu/uoc/gruizto/mybooks/activity/BookListActivity.java
@@ -1,8 +1,11 @@
 package edu.uoc.gruizto.mybooks.activity;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.LayoutRes;
 import android.support.design.widget.FloatingActionButton;
@@ -67,6 +70,18 @@ public class BookListActivity extends AppCompatActivity {
 
         if (findViewById(R.id.item_detail_container) != null) {
             mTwoPane = true;
+        }
+
+        // Set up channel for cloud notifications
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // Create channel to show notifications.
+            String channelId  = getString(R.string.default_notification_channel_id);
+            String channelName = getString(R.string.default_notification_channel_name);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            // TODO: lower priority after testing
+            NotificationChannel channel = new NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH);
+            notificationManager.createNotificationChannel(channel);
         }
 
         mViewModel = ViewModelProviders.of(this).get(AppViewModel.class);
