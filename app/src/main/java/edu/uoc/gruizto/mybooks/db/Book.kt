92a0d27de051b9aef456eced81d7ffcd4e261fa0
Gerard Ruiz <gruizto@uoc.edu>
2018-12-11 16:26:20 +0100

    Makes Book class Parcelable
    We intend to pass it to the frament that render the details

--- app/src/main/java/edu/uoc/gruizto/mybooks/db/Book.kt
+++ app/src/main/java/edu/uoc/gruizto/mybooks/db/Book.kt
@@ -1,17 +1,50 @@
 package edu.uoc.gruizto.mybooks.db
 
+import android.os.Parcel
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "mybooks")
-data class Book (
+class Book() : Parcelable {
+
     @PrimaryKey
-    var id: String,
-    var title: String? = null,
-    var author: String? = null,
-    var publicationDate: String? = null,
-    var description: String? = null,
+    var id: String = ""
+    var title: String? = null
+    var author: String? = null
+    var publicationDate: String? = null
+    var description: String? = null
     var coverUrl: String? = null
-) {
-    constructor():this("")
+
+    constructor(parcel: Parcel) : this() {
+        id = parcel.readString()
+        title = parcel.readString()
+        author = parcel.readString()
+        publicationDate = parcel.readString()
+        description = parcel.readString()
+        coverUrl = parcel.readString()
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(id)
+        parcel.writeString(title)
+        parcel.writeString(author)
+        parcel.writeString(publicationDate)
+        parcel.writeString(description)
+        parcel.writeString(coverUrl)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Book> {
+        override fun createFromParcel(parcel: Parcel): Book {
+            return Book(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Book?> {
+            return arrayOfNulls(size)
+        }
+    }
 }
